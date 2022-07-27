0ae18675b4b0ac3e7073e58aeeee4e005a59848b
Ben Bacon <benbacon@users.noreply.github.com>
2022-07-27 10:21:33 +0100

    Linode (#2)
    * Initial commit of Linode code
    
    * Change installed packages due to difference in Linode image~~
    
    Co-authored-by: Ben Bacon <github@baco.xyz>

---
+++ linode/variables.tf
@@ -0,0 +1,43 @@
+variable "domain" {
+
+}
+
+variable "ssh_ingress_ip" {
+
+}
+
+variable "ssh_path" {
+
+}
+
+variable "wireguard_private_key" {
+
+}
+
+variable "wireguard_public_key" {
+
+}
+
+variable "linode_api_token" {
+
+}
+
+variable "linode_image" {
+
+}
+
+variable "linode_type" {
+
+}
+
+variable "linode_region" {
+
+}
+
+variable "cloudflare_api_token" {
+
+}
+
+variable "cloudflare_zone_id" {
+
+}
